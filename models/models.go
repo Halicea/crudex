4e8b1d862d3f989e314c9ac1213f95634304f873
halicea <costa@codechem.com>
2024-06-19 23:10:20 +0200

    base model implementation

---
+++ models/models.go
@@ -0,0 +1,21 @@
+package models
+
+import "gorm.io/gorm"
+
+
+type IModel interface {
+	GetID() uint
+	SetID(id uint)
+}
+
+type BaseModel struct {
+	gorm.Model
+}
+
+func (self *BaseModel) GetID() uint {
+    return self.ID
+	
+func (self *BaseModel) SetID(id uint) {
+    self.ID = id
+}
+	
