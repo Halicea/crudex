cb0d2df059e179432fd64f37a320b7e5995e4c87
halicea <costa@codechem.com>
2024-07-04 21:18:26 +0200

    new example

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,36 +1,30 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
-	"github.com/halicea/crudex"
+	c "github.com/halicea/crudex"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	app := gin.New()                                             //create gin app
-	db, _ := gorm.Open(sqlite.Open("sample.db"), &gorm.Config{}) //create gorm db connection
-	db.AutoMigrate(&Car{}, &Driver{})                            //migrate the models
+	app := gin.New()
+	db, _ := gorm.Open(sqlite.Open("sample.db"), &gorm.Config{})
+	db.AutoMigrate(&Car{}, &Driver{})
 
-	conf := crudex.Setup() //setup global config
+	c.Setup(app, db).WithAutoScaffold(true).
+        WithScaffoldStrategy(c.ScaffoldStrategyIfNotExists).
+		Add(
+            c.New[Driver](), 
+            c.New[Car]()).
+		Index("gen/index.html")
 
-	var ctrls = []crudex.ICrudCtrl{ // create the controllers for the models
-		crudex.New[Car](db).OnRouter(app.Group("cars")).ScaffoldDefaults(),
-		crudex.New[Driver](db).OnRouter(app.Group("drivers")).ScaffoldDefaults(),
-	}
-	crudex.ScaffoldIndex(app, "gen/index.html", ctrls...) // create an index page that lists all the models
-
-	app.HTMLRender = crudex.NewRenderer() //set the renderer to the one provided by crudex
-
-	println(fmt.Sprintf("%v", conf))
-	app.Run(":8080") //run the app
+	app.HTMLRender = c.NewRenderer()
+	app.Run(":8080")
 }
 
 type Car struct {
-	crudex.BaseModel
-	// customize the input type and placeholder through the crud-input and crud-placeholder tags
+	c.BaseModel
 	Name        string `crud-input:"text" crud-placeholder:"Enter name"`
 	License     string `crud-input:"html" crud-placeholder:"Enter the license plate"`
 	Description string `crud-input:"wysiwyg" crud-placeholder:"Describe it"`
@@ -38,7 +32,7 @@ type Car struct {
 }
 
 type Driver struct {
-	crudex.BaseModel
+	c.BaseModel
 	Name  string
 	CarID uint
 	Car   Car `gorm:"foreignKey:CarID"`
